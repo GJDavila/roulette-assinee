fb121c1fde041e79742915df03127dfcd5f8242a
gjdavila <gjdavilaguillen@gmail.com>
2021-08-23 09:45:24 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Rulette from "./components/Rulette/Rulete.js";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Rulette />
       </header>
     </div>
   );
